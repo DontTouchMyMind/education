ff323603631c19bb6082535989c492577181fa79
Admin <tobigface@gmail.com>
2020-03-13 18:24:08 +0200

    added in_exam

---
+++ geekbrains/lesson4/in_exam.py
@@ -0,0 +1,11 @@
+hero = 'Superman'
+
+if hero.find('man') != -1:  # Если искомое будет отсутствовать, то рез.=-1
+    print('yes, it is')
+
+if 'man' in hero:
+    print('yes, it is')
+
+goals = ['стать гуру языка программирования', 'здоровье', 'накормить кота']
+if 'здоровье' in goals:
+    print('its ok')
