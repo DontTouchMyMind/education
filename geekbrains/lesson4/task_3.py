7925d9c2b0d2532fc2416369c5e306834d394319
Belousov <tobigface@gmail.com>
2020-03-14 11:52:40 +0200

    practical task number 3 was modified

--- geekbrains/lesson4/task_3.py
+++ geekbrains/lesson4/task_3.py
@@ -11,3 +11,10 @@ for num in my_list_1:
 new_list = my_list_1
 print(new_list)
 
+# Будем отслеживать кол-во входов
+numbers = [2, 2, 5, 12, 8, 2, 12]
+result = []
+for number in numbers:
+    if numbers.count(number) == 1:
+        result.append(number)
+print(result)
