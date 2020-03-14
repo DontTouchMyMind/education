e64e11b4787a371151fa93e19fbd13d8385091cc
Admin <tobigface@gmail.com>
2020-03-14 05:12:46 +0200

    14.03.20

--- geekbrains/lesson4/why_range.py
+++ geekbrains/lesson4/why_range.py
@@ -9,9 +9,18 @@ for winner in winners:
 # использовать while
 # или есть способ лучше?
 for i in range(len(winners)):
-    print(i)
-    print(winners[i])
-#
+    #print(i)
+    print(i+1, ')', winners[i])
+# Вывести нечетные цифры от 1 до 5
+numbers = [1, 3, 5]
+
+for number in numbers:
+    print(number)
+
+print(list(range(1, 20, 2)))
+
+for number in range(1, 20, 2):
+    print(number)
 #
 #
 #
