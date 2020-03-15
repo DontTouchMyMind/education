2e15e19680473fbd496629bd07766db90781880d
Admin <tobigface@gmail.com>
2020-03-15 14:55:22 +0200

    added the program "guess the number"

--- geekbrains/lesson 7/main.py
+++ geekbrains/lesson 7/main.py
@@ -7,3 +7,24 @@
 # Игра продолжается до тех пор пока компьютер не отгадает число.
 # Примечание: Знаки “=”, “>” и “<” пользователь вводит с клавиатуры для общения с компьютером.
 # Вы можете использовать этот способ или придумать свой.
+
+import random
+
+number = int(input('Enter a number between 0 and 100: '))
+# print(number)
+var_dialogs = None
+a = 1
+b = 100
+comp_number = random.randint(a, b)
+print(comp_number)
+while var_dialogs != '=':
+    var_dialogs = input('Enter your decision (Exam.">","<","="): ')
+    if var_dialogs == '<':
+        b = comp_number
+        comp_number = random.randint(a, b)
+        print(comp_number)
+    elif var_dialogs == '>':
+        a = comp_number
+        comp_number = random.randint(a, b)
+        print(comp_number)
+print('Its WIN')
