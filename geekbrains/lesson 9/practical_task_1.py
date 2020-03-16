c0d1571500f0fd1af84b75e39d4944dc3432d099
Belousov <tobigface@gmail.com>
2020-03-16 15:24:02 +0200

    was modified: added comments added validating of function

--- geekbrains/lesson 9/practical_task_1.py
+++ geekbrains/lesson 9/practical_task_1.py
@@ -1,9 +1,14 @@
 # Создайте функцию, принимающую на вход имя, возраст и город проживания человека.
 # Функция должна возвращать строку вида «Василий, 21 год(а), проживает в городе Москва»
 
+
 def main_func(name, age, city):
-    print(name, ',', age, 'год(а),', 'проживает в городе', city)
+    data = str((name, ',', age, 'год(а),', 'проживает в городе', city))
+    return data
 
 
-main_func('Василий', '21', 'Москва')
-print(type(main_func))
+# Validating of function
+var = main_func('Василий', '21', 'Москва')
+
+print(var)
+print(type(var))
