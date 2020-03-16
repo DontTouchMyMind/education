78b41a34ce475ca9936c5da24ecf09cb3095157e
Belousov <tobigface@gmail.com>
2020-03-16 10:57:18 +0200

    change the name of the argument to *args

--- geekbrains/lesson 8/args_kwargs.py
+++ geekbrains/lesson 8/args_kwargs.py
@@ -1,5 +1,5 @@
-def greating(say, *name):   # * - after '*' we can pass as many arguments one by one
-    print(say, name)        # The result will be a tuple of arguments
+def greating(say, *args):   # * - after '*' we can pass as many arguments one by one
+    print(say, args)        # The result will be a tuple of arguments
 
 
 greating('hello', 'Leo')
