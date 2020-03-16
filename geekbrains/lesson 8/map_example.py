8e9e64178416295edf36cd245c847b47a764685c
Belousov <tobigface@gmail.com>
2020-03-16 14:54:32 +0200

    created file, created functions, reformat to PEP 8

---
+++ geekbrains/lesson 8/map_example.py
@@ -0,0 +1,7 @@
+numbers = [5, 3, 4, 7, 8]
+
+# Get list of square roots of each numbers
+print(list(map(lambda x: x**2, numbers)))
+
+# Modify numbers to string
+print(list(map(lambda x: str(x), numbers)))
