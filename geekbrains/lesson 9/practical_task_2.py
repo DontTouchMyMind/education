96dfa3c2fb098f4793eadc3bf4ac9b93c34a8637
Belousov <tobigface@gmail.com>
2020-03-16 15:46:41 +0200

    created functions, reformat to PEP 8, added simple code

--- geekbrains/lesson 9/practical_task_2.py
+++ geekbrains/lesson 9/practical_task_2.py
@@ -1,1 +1,15 @@
 # Создайте функцию, принимающую на вход 3 числа и возвращающую наибольшее из них.
+numbers = [2, 4, 15]
+
+
+def max_func(a, b, c):
+    out = max(a, b, c)
+    return out
+
+
+max_func(numbers[0], numbers[1], numbers[2])
+
+print(max_func(numbers[0], numbers[1], numbers[2]))
+
+# Simple Code
+print(max(numbers[0], numbers[1], numbers[2]))
