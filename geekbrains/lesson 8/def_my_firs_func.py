ab0f9c3ed2ccadec1d67c1c92c27d3cd81f06ba8
Belousov <tobigface@gmail.com>
2020-03-16 09:22:41 +0200

    modified func: added inputs value of function

--- geekbrains/lesson 8/def_my_firs_func.py
+++ geekbrains/lesson 8/def_my_firs_func.py
@@ -1,15 +1,26 @@
 # Функция простой разделитель
 # Необходимо заменить повторяющие действия,
 # т.к. если в них будут ошибки, они могут множиться
-#
-def print_sep():    # название_функции (вводные параметры)
-    print('-' * 50) # тело функции
+# У функции нет возращаемого значения
+def print_sep(sep, sep_len):    # название_функции (вводные параметры)
+    print(sep * sep_len) # тело функции
 
-print('My first function')
-print_sep()
-print('Good seporators')
-print_sep()
-print('What is there are a lot of them?')
-print_sep()
-print_sep()
-print_sep()
+# print('My first function')
+# print_sep()
+# print('Good separators')
+# print_sep()
+# print('What is there are a lot of them?')
+# print_sep()
+# print_sep()
+# print_sep()
+
+# We can change the separator, if we will change input variable
+print_sep('*', 100)
+print_sep('-', 100)
+# we can change the separator and length
+print_sep('+', 50)
+result = print_sep('-', 100)
+print(result)   # Результат будет None(cм. строку 4)
+#
+#
+
