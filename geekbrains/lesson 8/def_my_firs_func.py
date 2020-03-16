6350bb5cda6d59c85077e9248fe8b5f35ea5a580
Belousov <tobigface@gmail.com>
2020-03-16 09:00:21 +0200

    created file, created first function

---
+++ geekbrains/lesson 8/def_my_firs_func.py
@@ -0,0 +1,15 @@
+# Функция простой разделитель
+# Необходимо заменить повторяющие действия,
+# т.к. если в них будут ошибки, они могут множиться
+#
+def print_sep():    # название_функции (вводные параметры)
+    print('-' * 50) # тело функции
+
+print('My first function')
+print('-' * 50)
+print('Good seporators')
+print('-' * 50)
+print('What is there are a lot of them?')
+print('-' * 50)
+print('-' * 50)
+print('-' * 50)
