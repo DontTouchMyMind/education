7635134661d958e80b0f7e00b7de23e4060c69cf
Belousov <tobigface@gmail.com>
2020-03-18 10:47:49 +0200

    created file, created functions, reformat to PEP 8

---
+++ geekbrains/lesson 12/open_file.py
@@ -0,0 +1,6 @@
+# Open the file for writing - file does not exists
+# f = open('first.txt', 'w')    # w - writing and file will be create
+# Open the file for reading - file does not exists
+# f = open('second.txt', 'r')    # r - reading
+# Open the file for reading - file exists
+f = open('first.txt', 'r')    # r - reading
