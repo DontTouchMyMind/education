5c295a5623bf13476f508dd0c2b5d8e41013afd7
Belousov <tobigface@gmail.com>
2020-03-18 13:01:44 +0200

    created file, created functions, reformat to PEP 8

---
+++ geekbrains/lesson 12/open_bytes.py
@@ -0,0 +1,10 @@
+# Open the file for writing bytes
+with open('bytes.txt', 'wb') as f:
+    pass
+
+with open('bytes.txt', 'rb') as f:
+    pass
+
+# Open the file in text mode with the encoding
+with open('bytes.txt', 'r', encoding='utf-8') as f:
+    pass
