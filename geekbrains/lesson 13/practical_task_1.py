f963b70849c0f4e04c4e67d26694c0a43041a50e
Admin <tobigface@gmail.com>
2020-03-18 22:53:10 +0200

    create of functions

--- geekbrains/lesson 13/practical_task_1.py
+++ geekbrains/lesson 13/practical_task_1.py
@@ -5,6 +5,27 @@
 #   ‘tracks’: [‘Последний месяц осени’, ‘Шапито’],
 #   ‘Albums’: [{‘name’: ‘Делать панк-рок’,‘year’: 2016},
 #   {‘name’: ‘Шапито’,‘year’: 2014}]}
-# С помощью модулей json и pickle сериализовать данный словарь в json и в байты, вывести результаты в терминал.
+# С помощью модулей json и pickle сериализовать данный словарь в json и в байты, вывести результаты
+# в терминал.
 # Записать результаты в файлы group.json, group.pickle соответственно.
 # В файле group.json указать кодировку utf-8.
+import pickle
+import json
+
+
+my_favourite_group = {
+  'name': 'Г.М.О.',
+  'tracks': ['Последний месяц осени', 'Шапито'],
+  'Albums': [
+      {'name': 'Делать панк-рок', 'year': 2016},
+      {'name': 'Шапито', 'year': 2014}
+  ]
+}
+
+with open('group.pickle', 'wb') as f:
+    pickle.dump(my_favourite_group, f)
+
+
+with open('group.json', 'w', encoding='utf-8') as f:
+    json.dump(my_favourite_group, f)
+
