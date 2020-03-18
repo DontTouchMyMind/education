71c481e18b1db6238ba78422b0348f893f962979
Belousov <tobigface@gmail.com>
2020-03-18 07:50:20 +0200

    created file, created functions, reformat to PEP 8

---
+++ geekbrains/lesson 10/sys_ex.py
@@ -0,0 +1,11 @@
+import sys
+
+
+print(sys.executable)
+# G:\Project\Python\geek\less_4\venv\Scripts\python.exe
+# C:\users\appdata\local\programs\python\...
+print(sys.platform)
+
+sys.exit()
+
+print('this code will not be run ')
