4adfd48137a91a3c941d8bba0d312a060baace0c
Belousov <tobigface@gmail.com>
2020-03-18 08:54:09 +0200

    created file, created functions, reformat to PEP 8

---
+++ geekbrains/lesson 10/practical_task_1.py
@@ -0,0 +1,23 @@
+# Создайте модуль (модуль - программа на Python, т.е. файл с расширением .py).
+# В нем создайте функцию создающую директории от dir_1 до dir_9 в папке из которой запущен данный код.
+# Затем создайте вторую функцию удаляющую эти папки.
+# Проверьте работу функций в этом же модуле.
+
+import os
+
+
+def create_folder():
+    for i in range(1, 10):
+        new_path = os.path.join(os.getcwd(), 'dir_{}'.format(i))
+        os.mkdir(new_path)
+
+
+def delete_folder():
+    for i in range(1, 10):
+        new_path = os.path.join(os.getcwd(), 'dir_{}'.format(i))
+        os.rmdir(new_path)
+
+
+create_folder()
+
+delete_folder()
