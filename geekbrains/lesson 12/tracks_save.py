fe45bbbef1f26e1a71cd34e6b3ff1cdc913ae47c
Belousov <tobigface@gmail.com>
2020-03-18 16:09:55 +0200

    created file, created functions, reformat to PEP 8

---
+++ geekbrains/lesson 12/tracks_save.py
@@ -0,0 +1,12 @@
+import json
+
+favorite_tracks =[
+    {'name': 'Вечная Любовь', 'artist': 'Агата Кристи'},
+    {'name': 'Angel', 'artist': 'Massive Attack'},
+    {'name': 'Jamming', 'artist': 'Bob Marley'}
+]
+
+with open('tracks.json', 'w', encoding='utf-8') as f:
+    json.dump(favorite_tracks, f)
+
+print('execute')
