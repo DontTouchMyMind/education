ed5fc25bb43132c68966579760693113f5e41ed9
Admin <tobigface@gmail.com>
2020-03-19 21:44:25 +0200

    creating functions

--- geekbrains/lesson 15/pragtical_task_2.py
+++ geekbrains/lesson 15/pragtical_task_2.py
@@ -5,3 +5,16 @@
 # Элемент не кратен 4.
 # Примечание: Список с целыми числами создайте вручную в начале файла.
 # Не забудьте включить туда отрицательные числа. 10-20 чисел в списке вполне достаточно.
+numbers = [-5, -4, -3, -2, -1, 0, 1, 2, 3, 4, 5, 45, 39, 28, 21, 20, 79, 88, -100500]
+
+# multiple 3
+result_mult = [number for number in numbers if number % 3 == 0]
+print(result_mult)
+
+# Positive elements
+result_pos = [number for number in numbers if number > 0]
+print(result_pos)
+
+# Not multiple 4
+result_not_mult = [number for number in numbers if number % 4 != 0]
+print(result_not_mult)
