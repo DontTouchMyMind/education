cab0f227f98ae606a8cc7d57ed1e85923152a610
Belousov <tobigface@gmail.com>
2020-03-19 11:34:23 +0200

    created file, created functions, reformat to PEP 8

--- geekbrains/lesson 14/example.py
+++ geekbrains/lesson 14/example.py
@@ -1,3 +1,14 @@
 # 1. Create a list from random numbers from 1 to 100
+import random
+
+numbers = [random.randint(1, 100) for i in range(10)]
+print(numbers)
+
 # 2. Create a list of square of numbers
+numbers = [number**2 for number in numbers]
+print(numbers)
 # 3. Create a list of names starting with letter A
+names = ['Anna', 'Max', 'Leo', 'Alice']
+
+names = [name for name in names if name.startswith('A')]
+print(names)
