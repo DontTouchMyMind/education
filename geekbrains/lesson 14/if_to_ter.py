4d94202a44809a4d982239d259943d918f79acec
Belousov <tobigface@gmail.com>
2020-03-19 10:01:06 +0200

    modify code with ternal operator

--- geekbrains/lesson 14/if_to_ter.py
+++ geekbrains/lesson 14/if_to_ter.py
@@ -1,13 +1,10 @@
 # слово -> СлОвО
-word = 'слово'
+word = 'рефрежератор'
 
 result = []
 
 for i in range(len(word)):
-    if i % 2 != 0:
-        letter = word[i].lower()
-    else:
-        letter = word[i].upper()
+    letter = word[i].lower() if i % 2 != 0 else word[i].upper()
     result.append(letter)
 
 result = ''.join(result)  # conversion from list to string
