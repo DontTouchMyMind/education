33c17d6dc7d3f34ea5fa41cfb887d878fb9ff79c
Belousov <tobigface@gmail.com>
2020-03-19 09:28:46 +0200

    created file, created functions, reformat to PEP 8

--- geekbrains/lesson 13/practical_task_2.py
+++ geekbrains/lesson 13/practical_task_2.py
@@ -1,2 +1,14 @@
 # Создать модуль music_deserialize.py. В этом модуле открыть файлы group.json и group.pickle,
 # прочитать из них информацию. И получить объект: словарь из предыдущего задания.
+import pickle
+import json
+
+with open('group.pickle', 'rb') as f:
+    group_pickle = pickle.load(f)
+
+
+with open('group.json', 'r', encoding='utf-8') as f:
+    group_json = json.load(f)
+
+print(group_json)
+print(group_pickle)
