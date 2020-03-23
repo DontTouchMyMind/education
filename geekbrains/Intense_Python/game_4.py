efbc552ef11fce741a42bfd12e0a172c25479c83
Belousov <tobigface@gmail.com>
2020-03-23 15:02:54 +0200

    added index

---
+++ geekbrains/Intense_Python/game_4.py
@@ -0,0 +1,35 @@
+import random
+
+words_list = ('автострада', 'бензин', 'инопланетянин', 'самолет', 'библиотека',
+              'шайба', 'олимпиада')
+
+secret_world = random.choice(words_list)
+print(secret_world)
+
+gamer_world = ['*'] * len(secret_world)  # Вывод - список
+print(''.join(gamer_world))  # Склеили строку, но в памяти он все равно список
+
+# gamer_world[1] = 'г'
+# print(''.join(gamer_world))
+error_counter = 0  # Количество попыток
+# Добавим диалог с пользователем
+while True:
+    letter = input('Введите одну букву').lower()  # Перевод в нижний регистр вне зависимости от ввода
+    if len(letter) != 1 or not letter.isalpha():  # Проверка длинны ввода и явл-ся ли буквой
+        continue
+
+    if letter in secret_world:
+        idx = 0
+        for symbol in secret_world:
+            print(idx, symbol)
+            idx += 1
+            gamer_world[idx] = letter
+    else:
+        error_counter += 1
+        print('Ошибок допущено:', error_counter)
+        if error_counter == 8:
+            print('Вы проиграли')
+            break
+    print(''.join(gamer_world))
+
+print('попробуйте еще')
