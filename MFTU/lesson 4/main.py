95a05c7a8a0c3eb4abb5cb87d7c8045d64e6334d
Belousov <tobigface@gmail.com>
2020-04-01 14:11:49 +0200

    added functions

--- MFTU/lesson 4/main.py
+++ MFTU/lesson 4/main.py
@@ -13,6 +13,14 @@ def max2(x, y):
         return y
 
 
+def max3(x, y, z):
+    return max2(x, max2(y, z))
+
+
+def hello_separated(name='World', separator='-'):
+    print('Hello', name, sep=separator)
+
+
 hello()  # Function call
 
 f = hello  # Synonym for function
@@ -23,4 +31,15 @@ hello_name('Max')
 
 var1 = int(input('Enter X: '))
 var2 = int(input('Enter Y: '))
+var3 = int(input('Enter Z: '))
+
 print(max2(var1, var2))
+print(max2('кит', 'кот'))
+print(max2('кот', 'котенок'))
+print(max3(var1, var2, var3))
+print(max3(3.4, 7, 1.1))
+print(max3('b', 'abc', 'abd'))
+hello_separated()
+hello_separated('Max', '***')
+hello_separated(separator='***')
+hello_separated(separator='***', name='John')
