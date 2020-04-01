e6958655b3caf9fde5da56efb235053a3a7e88af
Belousov <tobigface@gmail.com>
2020-04-01 08:21:53 +0200

    moved file

--- MFTU/prog_calc_sys.py
+++
@@ -1,8 +0,0 @@
-
-base = int(input('Enter your calculation system: '))
-x = int(input('Enter your number: '))
-
-while x > 0:            # "пока есть цифры"
-    digit = x % base    # Взять последнюю цифру "бери последнюю цифру"
-    print(digit, end='')# "Печатай ее"
-    x //= base          # Зачекнуть последнюю цифру "И отрывай ее"
