694da8493c475369a039ef8de904b6504d525aed
Belousov <tobigface@gmail.com>
2020-04-06 10:52:37 +0200

    added function for square

---
+++ MFTU/lesson 1/practice_turtle/ex_3.py
@@ -0,0 +1,18 @@
+import turtle
+
+corner = 0
+turtle.shape('turtle')
+
+# turtle.forward(100)
+# turtle.left(90)
+# turtle.forward(100)
+# turtle.left(90)
+# turtle.forward(100)
+# turtle.left(90)
+# turtle.forward(100)
+# turtle.left(90)
+
+while corner != 1000:  # Result is square
+    turtle.forward(100)
+    turtle.left(90)
+    corner += 1
