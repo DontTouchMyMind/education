acc44a97ecb3706074ba12e7a1884d764f12269b
Belousov <tobigface@gmail.com>
2020-04-10 15:04:15 +0200

    renamed input variable of function

--- MFTU/lesson 1/practice_turtle/ex_11.py
+++ MFTU/lesson 1/practice_turtle/ex_11.py
@@ -6,13 +6,13 @@ t.shape('turtle')
 t.speed(15)
 
 
-def circle(x):
+def circle(radius):
     t.seth(90)
     for i in range(90):
-        t.forward(x)
+        t.forward(radius)
         t.left(4)
     for i in range(90):
-        t.forward(x)
+        t.forward(radius)
         t.right(4)
 
 
