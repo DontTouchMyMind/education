410980c38ea5a6e6f385f614230f45b98c0a3313
Belousov <tobigface@gmail.com>
2020-04-10 11:30:35 +0200

    finished task and verified program

---
+++ MFTU/lesson 1/practice_turtle/ex_10.py
@@ -0,0 +1,21 @@
+# Draw a flower using circles
+
+import turtle as t
+
+angle = 0
+t.shape('turtle')
+t.speed(15)
+
+
+def circle():
+    for i in range(180):
+        t.forward(2)
+        t.left(2)
+    for i in range(180):
+        t.forward(2)
+        t.right(2)
+
+
+for i in range(60, 240, 60):
+    circle()
+    t.seth(i)
