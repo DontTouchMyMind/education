8cf1e49b700acfe8c91401144090b153d4969e0e
Belousov <tobigface@gmail.com>
2020-04-10 13:35:20 +0200

    finished task and verified program

---
+++ MFTU/lesson 1/practice_turtle/ex_12.py
@@ -0,0 +1,10 @@
+# Draw a spring. Use the function drawing an arc
+
+import turtle as t
+
+t.shape('turtle')
+t.left(90)
+for i in range(10):
+    t.circle(-50, 180, 100)
+    t.circle(-10, 180, 100)
+  
