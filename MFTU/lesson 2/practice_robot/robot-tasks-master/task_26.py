eef99af86a2c33b2c5af95f9cc35ba5a0729bc71
DontTouchMyMind <tobigface@gmail.com>
2020-04-16 10:49:12 +0200

    created function and verified task

--- MFTU/lesson 2/practice_robot/robot-tasks-master/task_26.py
+++ MFTU/lesson 2/practice_robot/robot-tasks-master/task_26.py
@@ -5,7 +5,31 @@ from pyrob.api import *
 
 @task(delay=0.02)
 def task_2_4():
-    pass
+    def chest():
+        fill_cell()
+        move_right()
+        fill_cell()
+        move_right()
+        fill_cell()
+        move_down()
+        move_left()
+        fill_cell()
+        move_up()
+        move_up()
+        fill_cell()
+
+    for n in range(5):
+        move_down()
+        chest()
+        for i in range(9):
+            move_right(3)
+            move_down()
+            chest()
+        move_left(37)
+        if n < 4:
+            move_down(4)
+
+    # move_left()
 
 
 if __name__ == '__main__':
