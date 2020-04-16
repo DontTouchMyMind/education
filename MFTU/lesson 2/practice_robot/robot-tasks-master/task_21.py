6fc9abd2b7494183ec0b238e039edcf21ffd657d
DontTouchMyMind <tobigface@gmail.com>
2020-04-16 09:50:14 +0200

    created function and verified task

--- MFTU/lesson 2/practice_robot/robot-tasks-master/task_21.py
+++ MFTU/lesson 2/practice_robot/robot-tasks-master/task_21.py
@@ -5,7 +5,19 @@ from pyrob.api import *
 
 @task(delay=0.05)
 def task_4_11():
-    pass
+    n = 0
+    move_right()
+    move_down()
+    fill_cell()
+    while n != 12:
+        move_down()
+        n += 1
+        for i in range(n):
+            fill_cell()
+            move_right()
+        fill_cell()
+        move_left(n)
+    move_down()
 
 
 if __name__ == '__main__':
--- MFTU/lesson 2/practice_robot/robot-tasks-master/task_22.py
+++ MFTU/lesson 2/practice_robot/robot-tasks-master/task_22.py
@@ -5,7 +5,24 @@ from pyrob.api import *
 
 @task
 def task_5_10():
-    pass
+    x = 0
+    y = 0
+    while not wall_is_on_the_right():
+        fill_cell()
+        x += 1
+        move_right()
+    while not wall_is_beneath():
+        fill_cell()
+        y += 1
+        move_down()
+    fill_cell()
+    while x != 0:
+        move_left()
+        for i in range(y):
+            fill_cell()
+            move_up()
+        move_down(y)
+        x -= 1
 
 
 if __name__ == '__main__':
