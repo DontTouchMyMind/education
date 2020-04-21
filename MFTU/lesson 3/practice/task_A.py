9f106e5f1ecf24223c2fdfdeff660ffc7ecaf051
DontTouchMyMind <tobigface@gmail.com>
2020-04-21 15:49:33 +0200

    created function and verified task

--- MFTU/lesson 3/practice/task_A.py
+++ MFTU/lesson 3/practice/task_A.py
@@ -1,10 +1,8 @@
 # The sum of the digits of a three-digit number
-
 number = int(input('Enter your number: '))
 result = 0
 while number > 0:
-    digit = number % 10
+    digit = int(number % 10)
     number //= 10
     result += digit
-
-print(f'The sum of digit is {result}')
+print(int(result))
