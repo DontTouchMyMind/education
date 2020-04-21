d8dd0c21cfb2589cb5a2a6fafb5fae2f27f8e16e
DontTouchMyMind <tobigface@gmail.com>
2020-04-21 08:13:27 +0200

    created function and verified task

--- MFTU/lesson 2/practice_robot/robot-tasks-master/task_32.py
+++ MFTU/lesson 2/practice_robot/robot-tasks-master/task_32.py
@@ -2,10 +2,39 @@
 
 from pyrob.api import *
 
+counter = 0
+
 
 @task(delay=0.01)
 def task_8_18():
-    pass
+    global counter
+
+    def go_to_up():
+        global counter
+        y = 0
+        while True:
+            if cell_is_filled():
+                counter += 1
+                y += 1
+            else:
+                fill_cell()
+                y += 1
+            if wall_is_above():
+                move_down(y)
+                break
+            move_up()
+
+    while not wall_is_on_the_right():
+
+        if wall_is_above() and wall_is_beneath():
+            fill_cell()
+        elif not wall_is_above():
+            move_up()
+            go_to_up()
+
+        move_right()
+    mov('ax', counter)
+    counter = 0
 
 
 if __name__ == '__main__':
