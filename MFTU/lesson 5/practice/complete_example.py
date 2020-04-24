2d67a3381043c0dc9bda73eca2e08cbbbd3631ff
DontTouchMyMind <tobigface@gmail.com>
2020-04-24 14:57:44 +0200

    created function and verified task

--- MFTU/lesson 5/practice/complete_example.py
+++ MFTU/lesson 5/practice/complete_example.py
@@ -12,18 +12,48 @@ velocity = Point(1, -2)
 
 
 def add(point_1, point_2):
+    """
+    Функция возвращает значение координат в следующий момент времени
+    :param point_1: int (x,y)
+    :param point_2: int (x,y)
+    :return: Сумма двух векторов
+    """
     new_point = Point(point_1.x + point_2.x,
                       point_1.y + point_2.y)
     return new_point
 
 
-def draw_circle(coords):
+def draw_ball(coords):
+    """
+    Функция отвечает за процесс отрисовки шарика
+    :param coords: int (x,y) - координаты положения шарика
+    """
     circle = Circle(coords, 10)
     circle.setFill('red')
 
     circle.draw(window)
 
 
+def clear_window():
+    """
+    Функция очистки экрана
+    """
+    rectangle = Rectangle(Point(0, 0), Point(SIZE_X, SIZE_Y))
+    rectangle.setFill('green')
+    rectangle.draw(window)
+
+
+def check_coords():
+    if coords.x < 0 or coords.x > SIZE_X:
+        velocity.x = - velocity.x
+    if coords.y < 0 or coords.y > SIZE_Y:
+        velocity.y = - velocity.y
+
+
 while True:
-    draw_circle(coords)
+    clear_window()
+    draw_ball(coords)
     coords = add(coords, velocity)
+
+    check_coords()
+    time.sleep(0.02)
