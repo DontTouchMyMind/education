172c89d3a60d43a2faea276b3b366c0d1a1eed9a
DontTouchMyMind <tobigface@gmail.com>
2020-04-24 15:19:48 +0200

    uniform gravity added

--- MFTU/lesson 5/practice/complete_example.py
+++ MFTU/lesson 5/practice/complete_example.py
@@ -9,6 +9,7 @@ window = GraphWin('Model', SIZE_X, SIZE_Y)
 coords = Point(200, 200)
 #  Вектор скорости
 velocity = Point(1, -2)
+acceleration = Point(0, 0.1)
 
 
 def add(point_1, point_2):
@@ -50,10 +51,20 @@ def check_coords():
         velocity.y = - velocity.y
 
 
+def update_velocity(velocity, acceleration):
+    return add(velocity, acceleration)
+
+
+def update_coords(coords, velocity):
+    return add(coords, velocity)
+
+
 while True:
     clear_window()
     draw_ball(coords)
-    coords = add(coords, velocity)
 
+    coords = update_coords(coords, velocity)
+    velocity = update_velocity(velocity, acceleration)
     check_coords()
+
     time.sleep(0.02)
