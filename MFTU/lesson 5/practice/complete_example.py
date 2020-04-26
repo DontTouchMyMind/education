542e734571eaa2070e1f48a8e50dd8bc6ca38efb
ohh <tobigface@gmail.com>
2020-04-26 12:18:10 +0200

    added central gravity

--- MFTU/lesson 5/practice/complete_example.py
+++ MFTU/lesson 5/practice/complete_example.py
@@ -1,15 +1,15 @@
 from graphics import *
 
-SIZE_X = 400
-SIZE_Y = 400
+SIZE_X = 800
+SIZE_Y = 800
 
 window = GraphWin('Model', SIZE_X, SIZE_Y)
 
 #  Начальное положение шарика
-coords = Point(200, 200)
+coords = Point(400, 700)
 #  Вектор скорости
-velocity = Point(1, -2)
-acceleration = Point(0, 0.1)
+velocity = Point(2, 0)
+acceleration = Point(0, 0)
 
 
 def add(point_1, point_2):
@@ -21,6 +21,12 @@ def add(point_1, point_2):
     """
     new_point = Point(point_1.x + point_2.x,
                       point_1.y + point_2.y)
+    return new_point
+
+
+def sub(point_1, point_2):
+    new_point = Point(point_1.x - point_2.x,
+                      point_1.y - point_2.y)
     return new_point
 
 
@@ -43,6 +49,10 @@ def clear_window():
     rectangle.setFill('green')
     rectangle.draw(window)
 
+    sun = Circle(Point(400, 400), 50)
+    sun.setFill('yellow')
+    sun.draw(window)
+
 
 def check_coords():
     if coords.x < 0 or coords.x > SIZE_X:
@@ -59,9 +69,20 @@ def update_coords(coords, velocity):
     return add(coords, velocity)
 
 
+def update_acceleration(ball_coords, center_coords):
+    diff = sub(ball_coords, center_coords)
+    distance_2 = (diff.x ** 2 + diff.y ** 2) ** (3 / 2)
+
+    G = 2000
+
+    return Point(-diff.x * G / distance_2, -diff.y * G / distance_2)
+
+
 while True:
     clear_window()
     draw_ball(coords)
+
+    acceleration = update_acceleration(coords, Point(400, 400))
 
     coords = update_coords(coords, velocity)
     velocity = update_velocity(velocity, acceleration)
