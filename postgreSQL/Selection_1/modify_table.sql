f4c4ba6fc7a707b52b6914231536bb6ccb81391f
Belousov <tobigface@gmail.com>
2020-05-12 13:51:12 +0200

    Add file

---
+++ postgreSQL/Selection_1/modify_table.sql
@@ -0,0 +1,11 @@
+/* Задача:
+    Объединить таблицы используя publisher_id
+    и получить объединенную таблицу
+*/
+
+ALTER TABLE book
+ADD COLUMN fk_publisher_id integer;
+
+ALTER TABLE book
+ADD CONSTRAINT fk_book_publisher 
+FOREIGN KEY(fk_publisher_id) REFERENCES publisher(publisher_id)
