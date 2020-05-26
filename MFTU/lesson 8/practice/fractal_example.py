7a4e2221a868cec7de283f027bffa4684815e639
Belousov_BPP <tobigface@gmail.com>
2020-05-26 08:47:42 +0200

    add fractal example

---
+++ MFTU/lesson 8/practice/fractal_example.py
@@ -0,0 +1,21 @@
+from turtle import *
+
+def draw(l, n):
+    if n == 0:
+        left(180)
+        return
+
+    x = l / (n + 1)
+    for i in range(n):
+        forward(x)
+        left(45)
+        draw(0.5 * x * (n - i - 1), n - i - 1)
+        left(90)
+        draw(0.5 * x * (n - i - 1), n - i - 1)
+        right(135)
+
+    forward(x)
+    left(180)
+    forward(l)
+
+draw(400, 3)
