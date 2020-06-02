f5b9069a8a5dd565680cafa3d5f2f460959f2c20
Belousov_BPP <tobigface@gmail.com>
2020-06-02 15:50:38 +0200

    Renamed file, added str method.

---
+++ geekbrains/lesson4/str.py
@@ -0,0 +1,13 @@
+friends = 'Максим Леонидович'
+print(friends[0])
+print(friends[1])
+print(friends[-1])
+
+print(len(friends))
+
+print(friends.find('Лео'))
+print(friends.split())
+print(friends.isdigit())
+print(friends.upper())
+print(friends.lower())
+help(list)
