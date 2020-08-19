ca955c583514b43fcc3f3baada75a00f149fd3be
DontTouchMyMind <tobigface@gmail.com>
2020-08-19 15:07:25 +0200

    Added files after viewing lesson.

---
+++ Udemy_Python3/#7. Moduls and componets/package_test.py
@@ -0,0 +1,7 @@
+from MainPackage import main_script
+# from MainPackage.SubPackage import subscript
+from MainPackage.SubPackage.subscript import hello_subscript
+
+main_script.hello_main()
+# subscript.hello_subscript()
+hello_subscript()
