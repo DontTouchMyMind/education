e52d3c388ef31a92af53f5da3188bd5bed74bcce
DontTouchMyMind <tobigface@gmail.com>
2020-08-26 15:41:51 +0200

    Added files after viewing lesson.

---
+++ Udemy_Python3/#8. Additionally/#104_f_str.py
@@ -0,0 +1,8 @@
+full_name = 'John Smith'
+print(f'full_name={full_name}')
+
+print(f'{full_name=}')
+print(f'Loading = {full_name=}')
+
+print(f'Logging: {full_name[5:]=}')
+print(f'Logging: {full_name=:>15}')
